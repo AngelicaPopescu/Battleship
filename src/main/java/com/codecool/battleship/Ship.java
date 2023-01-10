a243535bf7b6962afbd0110445a6c3eca2fba77e
razvanchirculescu <razvanchirculescu@gmail.com>
2023-01-10 11:58:35 +0200

    Ship.java work in progress

--- src/main/java/com/codecool/battleship/Ship.java
+++ src/main/java/com/codecool/battleship/Ship.java
@@ -1,4 +1,53 @@
 package com.codecool.battleship;
 
+import java.util.List;
+
 public class Ship {
+
+    private List<Square>squareList;
+    private Ship shipType;
+
+
+    public Ship (List<Square> squareList ){
+        this.squareList = squareList;
+    }
+
+
+    public List<Square> getSquareList() {
+        return squareList;
+    }
+
+
+    public Ship getShipType() {
+        return shipType;
+    }
+
+    public void setShipType(Ship shipType) {
+        this.shipType = shipType;
+    }
+
+    public boolean isShipPosition(Square square){
+        if(squareList != null){
+            for (Square s: squareList){
+                if (s.getY() == square.getY() && s.getX() == square.getX()) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public boolean isSunk(){
+
+        if(squareList != null){
+            for (Square s: squareList){
+                if(s.getSquareStatus == SquareStatus.SHIP){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+
 }
