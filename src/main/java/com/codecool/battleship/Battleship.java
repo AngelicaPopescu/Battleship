f04578c564c712819f390378904e2d24d33d9c34
BAdrian0 <adibalasu@gmail.com>
2023-01-11 10:02:43 +0200

    setSquareStatus

--- src/main/java/com/codecool/battleship/Battleship.java
+++ src/main/java/com/codecool/battleship/Battleship.java
@@ -4,29 +4,21 @@ import java.util.Arrays;
 
 public class Battleship {
     public static void main(String[] args) {
-//        while (true) {
-//            Display.displayWelcomeMessage();
-//            Display.displayGameModeOptions();
-//            int gameMode = Input.getGameMode();
-//            if (gameMode == 3) {
-//                System.exit(0);
-//            } else if (gameMode == 1) {
-//                Game game = new Game();
-//                game.play();
-//            }
-//        }
+        while (true) {
+            Display.displayWelcomeMessage();
+            Display.displayGameModeOptions();
+            int gameMode = Input.getGameMode();
+            if (gameMode == 3) {
+                System.exit(0);
+            } else if (gameMode == 1) {
+                Game game = new Game();
+                game.play();
+            }
+        }
 
 
         //testing
-        int boardSize = 5;
-//        Board board = new Board();
-        BoardFactory board = new BoardFactory();
-        board.setOcean(boardSize);
-        System.out.println("Ocean: "+ Arrays.deepToString(board.getOcean()));
-        board.boardDisplay();
-        System.out.println();
-        board.randomPlacement(ShipType.DESTROYER, boardSize);
-        board.boardDisplay();
+//
 
 //        while (true) {
 //            Display.displayWelcomeMessage();
--- src/main/java/com/codecool/battleship/Square.java
+++ src/main/java/com/codecool/battleship/Square.java
@@ -18,21 +18,26 @@ public class Square {
     //The Square class has a SquareStatus field
     SquareStatus squareStatus;
 
+    //get
     public SquareStatus getSquareStatus(){
         return squareStatus;
     }
 
+    //SET
+    public void setSquareStatus(SquareStatus squareStatus) {
+        this.squareStatus = squareStatus;
+    }
 
     public Square(int x, int y, SquareStatus status){
         X = x;
         Y = y;
-        squareStatus = status;
+        this.squareStatus = status;
     }
 
     //The Square class has method that returns a graphical representation of SquareStatus
     public char graphicalSquareStatus() {
         return switch (this.squareStatus) {
-            case EMPTY, HIT, SHIP, MISSED -> this.squareStatus.GetCharacter();
+            case EMPTY, HIT, SHIP, MISSED, SUNK -> this.squareStatus.GetCharacter();
         };
     }
 
